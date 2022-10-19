bd7ee68e12e492a5aee63a4dc4bafee872bb2aa3
Emir Kenar <emirkenar@emirs-mbp.local>
2022-10-20 01:10:38 +0300

    fix: remove build from gitignore

---
+++ build/CMakeFiles/robotics.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robotics.dir/src/main.cpp.o"
+  "CMakeFiles/robotics.dir/src/main.cpp.o.d"
+  "CMakeFiles/robotics.dir/src/transforms.cpp.o"
+  "CMakeFiles/robotics.dir/src/transforms.cpp.o.d"
+  "robotics"
+  "robotics.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robotics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
