fa708a1ed4b1632035148154a1ff3d86d6a73c88
Emir Kenar <emirkenar@emirs-mbp.local>
2022-10-23 01:30:38 +0300

    feat: ik wip

---
+++ include/inverse_kinematics.h
@@ -0,0 +1,41 @@
+#ifndef INVERSE_KINEMATICS_H
+#define INVERSE_KINEMATICS_H
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <Eigen/Core>
+//#include <eigen3/Eigen/Core>
+
+struct VectorSpaceVel
+{
+    double x_dot;
+    double y_dot;
+    double z_dot;
+    double w_x;
+    double w_y;
+    double w_z;
+};
+
+class IKModel
+{
+    private:
+        int dof_;   // degree of freedom
+        std::vector<Eigen::Matrix4d> transformation_matrices_;
+        Eigen::Matrix4d homogeneous_trans_matrix_;
+        Eigen::MatrixXd total_tool_displacement_;
+        std::vector<double> joint_space_variables_;
+        std::vector<double> joint_space_output_;
+
+        Eigen::MatrixXd jacobian_;
+
+    public:
+        IKModel(const int& number_of_joints);
+        void getJacobianMatrix();
+        Eigen::MatrixXd getJacobianColumn(const int& joint_idx);
+        Eigen::Matrix4d getHomogeneousTransUntil(const int& joint_idx);
+        
+
+};
+
+#endif // INVERSE_KINEMATICS_H
