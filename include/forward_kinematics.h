04aa3867412d4422d74f8634e2db17038d888e8f
Emir Kenar <emirkenar@emirs-mbp.local>
2022-10-22 23:03:42 +0300

    feat: get trans. matrices for further use

--- include/forward_kinematics.h
+++ include/forward_kinematics.h
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <vector>
 #include <Eigen/Core>
+// #include <eigen3/Eigen/Core>
 
 struct AxisDHParam
 {
@@ -20,12 +21,15 @@ class FKModel
         int dof_;   // degree-of-freedom
         std::vector<AxisDHParam> dh_params_;
         std::vector<double> variable_joint_states_;
+        std::vector<Eigen::Matrix4d> transformation_matrices_;
         Eigen::Matrix4d fk_model_;
 
     public:
         FKModel(const int& number_of_joints);
         void assignDHParameters(const int& joint_idx, const AxisDHParam& params);
+        void assignTransformationMatrices();
         Eigen::Matrix4d getHomogeneousTransMatrix(const AxisDHParam& dh_params);
+        std::vector<Eigen::Matrix4d> getTransformationMatrices();
         void calculateFKModel();
         Eigen::Matrix4d getFKModel();
 
--- src/forward_kinematics.cpp
+++ src/forward_kinematics.cpp
@@ -4,6 +4,7 @@ FKModel::FKModel(const int& number_of_joints) : dof_(number_of_joints)
 {
     dh_params_.reserve(dof_);
     variable_joint_states_.reserve(dof_);
+    transformation_matrices_.reserve(dof_);
     fk_model_ = Eigen::Matrix4d::Identity(4,4);
 }
 
@@ -12,12 +13,11 @@ void FKModel::assignDHParameters(const int& joint_idx, const AxisDHParam& params
     dh_params_.at(joint_idx) = params;
 }
 
-void FKModel::calculateFKModel()
+void FKModel::assignTransformationMatrices()
 {
-    fk_model_ = Eigen::Matrix4d::Identity(4,4);
-    for (auto joint: dh_params_)
+    for (int i = 0; i < dof_; i++)
     {
-        fk_model_ *= this->getHomogeneousTransMatrix(joint);
+        transformation_matrices_.at(i) = this->getHomogeneousTransMatrix(dh_params_.at(i));
     }
 }
 
@@ -39,6 +39,20 @@ Eigen::Matrix4d FKModel::getHomogeneousTransMatrix(const AxisDHParam& dh_param)
     A(2,3) = dh_param.d;
 
     return A;
+}
+
+std::vector<Eigen::Matrix4d> FKModel::getTransformationMatrices()
+{
+    return transformation_matrices_;
+}
+
+void FKModel::calculateFKModel()
+{
+    fk_model_ = Eigen::Matrix4d::Identity(4,4);
+    for (auto joint: transformation_matrices_)
+    {
+        fk_model_ *= joint;
+    }
 }
 
 Eigen::Matrix4d FKModel::getFKModel()
